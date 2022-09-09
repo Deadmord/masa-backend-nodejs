a6dbfa5c7f8d8822abcb581882c41ac2d66201b7
Nikolay Belokonenko <belokonenkons@gmail.com>
2022-09-09 19:24:12 +0300

    NP-5 Result in brauser

--- source/controllers/school.controller.ts
+++ source/controllers/school.controller.ts
@@ -1,12 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
+import { whiteBoardType } from '../entities';
 import { SchoolService } from '../services/school.service';
 
 const schoolService: SchoolService = new SchoolService();
 
 const getBoardTypes = async (req: Request, res: Response, next: NextFunction) => {
-    return res.status(200).json({
-        message: schoolService.getBoardTypes()
-    });
+    schoolService.getBoardTypes()
+        .then((result: whiteBoardType[]) => {
+            return res.status(200).json({
+                message: result
+            });
+        })
 };
 
 export default { getBoardTypes };
--- source/services/school.service.ts
+++ source/services/school.service.ts
@@ -7,31 +7,32 @@ interface localWhiteBoardType {
     white_board_type: string;
 }
 interface ISchoolService {
-    getBoardTypes(): string;
+    getBoardTypes(): Promise<whiteBoardType[]>;
 }
 
 export class SchoolService implements ISchoolService {
-    public getBoardTypes(): string {
-        const sql: SqlClient = require("msnodesqlv8");
-
-        const connectionString: string = "server=.;Database=masa_school;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
-        const query: string = "SELECT * FROM white_board_type";
-
-        sql.open(connectionString,  (connectionError: Error, connection: Connection) => {
-            connection.query(query, (queryError: Error | undefined, queryResult: localWhiteBoardType[] | undefined) => {
-                const result: whiteBoardType[] = [];
-                if (queryResult !== undefined) {
-                    queryResult.forEach((whiteBoardType: localWhiteBoardType) => {
-                        result.push(
-                            this.parseLocalWhiteBoardType(whiteBoardType)
-                        ); 
-                    });
-                }
-                console.log(result);
-             })
+    
+    public getBoardTypes(): Promise<whiteBoardType[]> {
+        return new Promise<whiteBoardType[]>((resolve , reject) => {
+            const result: whiteBoardType[] = [];
+            const sql: SqlClient = require("msnodesqlv8");
+    
+            const connectionString: string = "server=.;Database=masa_school;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+            const query: string = "SELECT * FROM white_board_type";
+    
+            sql.open(connectionString,  (connectionError: Error, connection: Connection) => {
+                connection.query(query, (queryError: Error | undefined, queryResult: localWhiteBoardType[] | undefined) => {  
+                    if (queryResult !== undefined) {
+                        queryResult.forEach((whiteBoardType: localWhiteBoardType) => {
+                            result.push(
+                                this.parseLocalWhiteBoardType(whiteBoardType)
+                            ); 
+                        });
+                    }
+                    resolve(result);
+                 })
+            });
         });
-
-        return "getBoardTypes";
     }
 
     private parseLocalWhiteBoardType(local: localWhiteBoardType): whiteBoardType {
